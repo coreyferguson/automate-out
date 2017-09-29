755eb60847bc75d0ce6546c5db94852dd31e220d
Corey Ferguson <corey_ferguson@intuit.com>
2017-09-28 22:56:35 -0700

    deleted first forkLift implementation

--- src/user/forkLiftFirstImpl.js
+++ src/user/forkLiftFirstImpl.js
@@ -4,23 +4,9 @@ class ForkLiftFirstImpl {
   getVector(game, sprite) {
     const { math, physics, space } = game;
     const { resources } = space;
-    if (resources && resources[0]) {
-    	const resource = resources[0];
-    	const radians = physics.arcade.angleToXY(sprite, resource.x, resource.y);
-    	const distance = physics.arcade.distanceToXY(sprite, resource.x, resource.y);
-    	if (distance > 10) {
-	      return {
-	        direction: math.radToDeg(radians),
-	        magnitude: 100
-	      };
-    	} else {
-    		return {
-    			direction: 0,
-    			magnitude: 0
-    		};
-    	}
-    } else {
-      return null;
+    return {
+      direction: 0, // angle, in degrees, to aim your saucer
+      magnitude: 0 // speed, >=0, <=100
     }
   }
 
