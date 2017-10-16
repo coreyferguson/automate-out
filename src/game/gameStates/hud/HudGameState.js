9aaaaaf1b6e3d3735b292a0b17759f47730e1165
Corey Ferguson <corey_ferguson@intuit.com>
2017-10-15 20:31:19 -0700

    full screen button

--- src/game/gameStates/hud/HudGameState.js
+++ src/game/gameStates/hud/HudGameState.js
@@ -3,7 +3,29 @@ const ioc = require('../../../ioc');
 
 class HudGameState {
 
+  preload() {
+    ioc.game.phaserGame.load.image('full-screen', 'icons/full-screen.png');
+  }
+
   create() {
+    // full screen
+    this.fullScreenIcon = ioc.game.phaserGame.add.sprite(
+      ioc.game.phaserGame.width-50,
+      ioc.game.phaserGame.height-50,
+      'full-screen');
+    this.fullScreenIcon.width = 50;
+    this.fullScreenIcon.height = 50;
+    this.fullScreenIcon.fixedToCamera = true;
+    this.fullScreenIcon.inputEnabled = true;
+
+    this.fullScreenIcon.events.onInputDown.add(() => {
+      if (ioc.game.phaserGame.scale.isFullScreen) {
+        ioc.game.phaserGame.scale.stopFullScreen();
+      } else {
+        ioc.game.phaserGame.scale.startFullScreen(false);
+      }
+    });
+
     // iron
     this.ironCounterIcon = ioc.game.phaserGame.add.sprite(10, 10, 'iron');
     this.ironCounterIcon.fixedToCamera = true;
